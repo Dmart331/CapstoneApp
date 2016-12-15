fb51e6cbbbfa06a3158e8d40d1b26943fe0c5b6e
Paul Martin <Dmart331@gmail.com>
2016-12-15 09:42:25 -0600

    Added android code block

--- www/js/TrafficCtrl.js
+++ www/js/TrafficCtrl.js
@@ -1,4 +1,13 @@
 app.controller('TrafficCtrl', ['$scope', function($scope, $ionicLoading, $compile) {
+    $scope.disableTap = function() {
+var container = document.getElementsByClassName('pac-container');
+angular.element(container).attr('data-tap-disabled', 'true');
+var backdrop = document.getElementsByClassName('backdrop');
+angular.element(backdrop).attr('data-tap-disabled', 'true');
+angular.element(container).on("click", function() {
+document.getElementById('pac-input').blur();
+});
+};
 
     var myLatLng;
     function initialize() {
