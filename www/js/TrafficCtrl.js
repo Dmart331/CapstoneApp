3485cc5aa6c807c5b6c825035884faf76253ace0
Paul Martin <Dmart331@gmail.com>
2016-12-14 22:51:24 -0600

    "added autocomplete and address search"

--- www/js/TrafficCtrl.js
+++ www/js/TrafficCtrl.js
@@ -1,29 +1,63 @@
-app.controller('TrafficCtrl', function($scope, $ionicLoading, $compile) {
-        
+app.controller('TrafficCtrl', ['$scope', function($scope) {
+    var myLatLng;
+    function initialize() {
 
-        var myLatLng;
-        function initialize(){
-        	navigator.geolocation.getCurrentPosition(function(position){
-        		myLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        navigator.geolocation.getCurrentPosition(function(position){
+            myLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        })
+        var mapOptions = {
+            center: myLatLng,
+            zoom: 16,
+            disableDefaultUI: true,// DISABLE MAP TYPE
+            scrollwheel: true
+        };
+        var map = new google.maps.Map(document.getElementById('map'),
+            mapOptions);
+        var input = /** @type {HTMLInputElement} */ (
+            document.getElementById('pac-input'));
+        // Create the autocomplete helper, and associate it with
+        // an HTML text input box.
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', map);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        var infowindow = new google.maps.InfoWindow();
+        var marker = new google.maps.Marker({
+            map: map
+        });
 
-        		var mapOptions = {
-        			center: myLatLng,
-        			zoom: 16,
-        			mapTypeId: google.maps.MapTypeId.ROADMAP
-        		};
-        		var map = new google.maps.Map(document.getElementById('map'),
-        			mapOptions);
-
-        		var marker = new google.maps.Marker({
-        			position: myLatLng,
-        			map: map,
-        			title: 'Your Pos'
-        		});
-
-        		var trafficLayer = new google.maps.TrafficLayer();
+        var trafficLayer = new google.maps.TrafficLayer();
         trafficLayer.setMap(map);
-        		$scope.map = map;
-        	});
-        }
-        ionic.Platform.ready(initialize)
-    });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map, marker);
+        });
+        // Get the full place details when the user selects a place from the
+        // list of suggestions.
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+            infowindow.close();
+            var place = autocomplete.getPlace();
+            if (!place.geometry) {
+                map.setCenter(myLatLng)
+            }
+            if (place.geometry.viewport) {
+                map.fitBounds(place.geometry.viewport);
+            } else {
+                map.setCenter(place.geometry.location);
+                map.setZoom(16);
+            }
+            // Set the position of the marker using the place ID and location.
+            marker.setPlace( /** @type {!google.maps.Place} */ ({
+                placeId: place.place_id,
+                location: place.geometry.location
+            }));
+            console.log("place", place);
+            marker.setVisible(true);
+            infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+                '<br>' +
+                place.formatted_address + '</div>');
+            infowindow.open(map, marker);
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+    // Run the initialize function when the window has finished loading.
+}])
