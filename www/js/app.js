e95de9f66894755ab235027f88ac917ef5a041c1
Paul Martin <Dmart331@gmail.com>
2017-01-01 19:49:13 -0600

    fixing ionic bug

--- www/js/app.js
+++ www/js/app.js
@@ -23,7 +23,6 @@ var app = angular.module('TrafficApp', ['ionic' , 'angularMoment'])
   });
 })
 
-
 .config(function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('side/home')
 
@@ -51,7 +50,7 @@ var app = angular.module('TrafficApp', ['ionic' , 'angularMoment'])
     }
   })
   .state('sidemenu.reddit' , {
-    url:'/Reddit',
+    url:'/reddit',
     views: {
       'menuContent' :{
         controller: 'RedditCtrl',
@@ -60,7 +59,7 @@ var app = angular.module('TrafficApp', ['ionic' , 'angularMoment'])
     }
   })
   .state('sidemenu.weather' , {
-    url:'/Weather',
+    url:'/weather',
     views: {
       'menuContent' :{
         controller: 'WeatherCtrl',
@@ -70,16 +69,3 @@ var app = angular.module('TrafficApp', ['ionic' , 'angularMoment'])
   })
 })
 
-  var commands = {
-                'show me *tag': function(tag) {
-                    changeView(tag);
-                }
-            }
-
-            annyang.addCommands(commands);
-            annyang.debug();
-            annyang.start();
-
-var changeView = function(tag){
-
-}
